c22383850c9c680a7ce14316c20945f462984a9a
Bharath <bharath.kadarla@digiscloud.com>
2017-12-02 16:22:24 +0530

    New File Added

---
+++ src/main/java/MavenBuild.java
@@ -0,0 +1,12 @@
+
+public class MavenBuild {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class is build from Jenkins !!!");
+	}
+
+}
